3cec1f0d678c2387cd8f3b38d255400945581b00
Kali Edgar1998lml <EDGA>
2021-12-03 00:52:17 -0500

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	Cursos/Python/Leccion01/main.py

--- Cursos/Python/Leccion01/main.py
+++ Cursos/Python/Leccion01/main.py
@@ -4,3 +4,24 @@ print("Hola Mundo")
 # El objetivo de este ejercicio es imprimir un mensaje: Saludos desde Python
 
 print("Saludos desde Python")
+
+
+#Declarar Variables en python (Las variables no es necesario declarar el tipo
+
+miVariable = "Hola desde Python Variable"
+miVariableNum = 5
+miVariableBol = True
+
+print(miVariable)
+print(miVariableNum)
+print(miVariableBol)
+
+#Variables con operaciones
+
+x = 10
+y = 2
+z = x + y
+print(z)
+a = 11
+p = z + a
+print(p)
