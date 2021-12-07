c71e68b25540a07a05dbd75aab317e7d0b72ab36
edgar1998lml <edgar-efra@hotmail.com>
2021-12-06 21:34:05 -0600

    commit gitignore

---
+++ Cursos/Python/Leccion04/main.py
@@ -0,0 +1,16 @@
+#Actividad while
+condicion = False
+
+while condicion:
+    print('Ejecutando ciclo while')
+else:
+    print('fin del comando while')
+
+#Actividad
+
+contador = 0
+while contador < 3:
+    print(contador)
+    contador = contador + 1
+else:
+    print('Fin del ciclo')
