b6f4643fba26120bd30d69aeb97714258b04cb79
edgar1998lml <53629257+edgar1998lml@users.noreply.github.com>
2021-12-11 00:08:39 -0600

    Del 1.0
    Se añade delete funcional

--- Cursos/flask_curso_Youtube/app.py
+++ Cursos/flask_curso_Youtube/app.py
@@ -7,23 +7,27 @@ from flask import request, redirect, url_for, flash
 app = Flask(__name__)
 
 #mysqlConnection linux
-app.config['MYSQL_HOST'] = '127.0.0.1'
+#app.config['MYSQL_HOST'] = '127.0.0.1'
 #mySqlConnectio Windows 10
-#app.config['MYSQL_HOST'] = 'Localhost'
+app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
 #Esta es para la db de linux
-app.config['MYSQL_PASSWORD'] = '--A]NGcKf/3](T2M'
+#app.config['MYSQL_PASSWORD'] = '--A]NGcKf/3](T2M'
 #Esta es para la db de windows10
-#app.config['MYSQL_PASSWORD'] = 'password'
+app.config['MYSQL_PASSWORD'] = 'password'
 app.config['MYSQL_DB'] = 'flaskcontacts'
 mysql = MySQL(app)
 
 #Settings
-app.secret_key = 'Mi Clave Super Mega Segura'.encode('utf-8')
+app.secret_key = 'Mi_Clave_Super_Mega_Segura'.encode('utf-8')
 
 @app.route('/')
 def Index():
-    return render_template('index.html')
+    cur = mysql.connection.cursor()
+    cur.execute('SELECT * FROM contacts')
+    data = cur.fetchall()
+    print(data)
+    return render_template('index.html', contacts = data)
 
 @app.route('/add_contact', methods=['POST'])
 def add_contact():
@@ -44,9 +48,14 @@ def add_contact():
 def edit_contact():
     return 'Edit contact'
 
-@app.route('/delete')
-def delete():
-    return 'Delete'
+@app.route('/delete/<string:id>')
+def delete_contact(id):
+   #return (id)
+   cur = mysql.connection.cursor()
+   cur.execute('DELETE FROM contacts WHERE id ={0}'.format(id))
+   mysql.connection.commit()
+   flash('Contact Removed Successfully')
+   return redirect(url_for('Index'))
 
 if __name__ == '__main__':
     app.run(port=3000, debug=True)
